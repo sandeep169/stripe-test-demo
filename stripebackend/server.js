c1a973ca91db2842c8d124c4bda17003ebce5fb3
sandeeprawat <sandeeprawat169@gmail.com>
2022-02-07 15:25:34 +0530

    updates

--- stripebackend/server.js
+++ stripebackend/server.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const express = require("express");
 
 // This is your test secret API key.
-const stripe = require('stripe')('sk_test_51KMn2fSDxEXjdd9VaNEwe6ilQUixVxUJ022BZLnSzJrAO4c1SrGBQ9HZwqYksazhXkuvIXQhaOV2jKbxMI2fLwga00fGOdHDkD')
+const stripe = require('stripe')('sk_test_51KMn2fSDxEXjdd9VFt2R9fQJJC6T9r1M58VCRPcxZfmAWfrtzzZxRb3biOqJBFG6ilJHSppNGhLJuDYrQ1t1ahHR00o8USmrbC')
 // const {v4}  = require("uuid")
 const { v4: uuidv4 } = require("uuid")
 
@@ -22,21 +22,74 @@ const calculateOrderAmount = (items) => {
     return 1400;
 };
 
+
+const YOUR_DOMAIN = 'http://localhost:3000';
+
+
+// const stripe = require('stripe')('sk_test_51KMn2fSDxEXjdd9VFt2R9fQJJC6T9r1M58VCRPcxZfmAWfrtzzZxRb3biOqJBFG6ilJHSppNGhLJuDYrQ1t1ahHR00o8USmrbC');
+
+// const product = await stripe.products.create({name: 'T-shirt'});
+// const price = await stripe.prices.create({
+//     product: '{{PRODUCT_ID}}',
+//     unit_amount: 2000,
+//     currency: 'inr',
+//   });
+
+app.post('/create-checkout-session', async (req, res) => {
+
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        customer: 'cus_L49MMGnHsNUDmm',
+        // Country:'US',
+        // customer_email:'sandeep.mamsys@mamsys.com',
+        // payment_method: 'pm_1KO0fRSDxEXjdd9VPqSQCNj9',
+        line_items: [
+            {
+                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                price: 'price_1KO17SSDxEXjdd9Ve0Tc6VkP',
+                quantity: 1,
+            },
+        ],
+        mode: 'payment',
+        success_url: `${YOUR_DOMAIN}/?success=true`,
+        cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    });
+
+    res.redirect(303, session.url);
+});
+
 app.post("/create-payment-intent", async (req, res) => {
     const { items } = req.body;
-
     // Create a PaymentIntent with the order amount and currency
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: calculateOrderAmount(items),
-        currency: "eur",
-        automatic_payment_methods: {
-            enabled: true,
-        },
-    });
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: calculateOrderAmount(items),
+            // customer: 'cus_L49MMGnHsNUDmm',
+            // payment_method: 'pm_1KO0fRSDxEXjdd9VPqSQCNj9',
+            // confirm:true,
+            currency: "eur",
+            // payment_method_types: [
+            //     "card",
+            //     "acss_debit",
+            //   ],
+            // payment_method_types: [
+            //     // 'bancontact',
+            //     'card',  
+            // ],
+          
+            // automatic_payment_methods: {
+            //     enabled: true,
+            // },
+        });
+        // console.log(res)
+        res.send({
 
-    res.send({
-        clientSecret: paymentIntent.client_secret,
-    });
+            clientSecret: paymentIntent.client_secret,
+        });
+    } catch (err) {
+        console.log(err)
+    }
 });
 
 app.listen(4242, () => console.log("Node server listening on port 8282!"));
